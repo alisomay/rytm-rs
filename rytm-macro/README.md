83f774e78d0dc82e767bfbba26fb5a8eb61bb169
Ali Somay <alisomay@runbox.com>
2023-12-14 17:06:40 +0100

    Organize cargo files

---
+++ rytm-macro/README.md
@@ -0,0 +1,4 @@
+# rytm-macro
+
+Exposes a few proc macros for generating code for the [rytm-rs](https://github.com/alisomay/rytm-rs) crate.
+It is not intended to be used directly but rather through the [rytm-rs](https://github.com/alisomay/rytm-rs) crate.
--- rytm-macro/src/lib.rs
+++ rytm-macro/src/lib.rs
@@ -12,8 +12,18 @@ use syn::{
     parse_macro_input, punctuated::Punctuated, token::Comma, DeriveInput, Error as SynError, ItemFn,
 };
 
-/******** Parameter Range *********/
-
+/// A proc macro to apply range validation for function parameters used in `rytm-rs`.
+///
+/// Please check `rytm-rs` source code for exhaustive usage examples.
+///
+/// # Example
+///
+/// ```
+/// #[parameter_range(range = "arg_name:0..=127")]
+/// #[parameter_range(range = "arg_name:0..=127:u16")]
+/// #[parameter_range(range = "arg_name[opt]:0..=127")]
+/// #[parameter_range(range = "arg_name:0..=127", range = "another_arg_name:0.4..=127.8")]
+/// ```
 #[proc_macro_attribute]
 pub fn parameter_range(args: TokenStream, input: TokenStream) -> TokenStream {
     let input_fn = parse_macro_input!(input as ItemFn);
@@ -125,8 +135,28 @@ pub fn parameter_range(args: TokenStream, input: TokenStream) -> TokenStream {
     result.into()
 }
 
-/******** Machine Parameters *********/
-
+/// A proc macro to implement normal and parameter lock getter, setter and clearer methods mor machine parameter structs.
+///
+/// `#<number>` annotations denote synth parameter indices used internally.
+///
+/// The macro also generates a `apply_to_raw_sound_values` method which is used to apply the parameter values to the raw sound struct.
+///
+/// Please check `rytm-rs` source code for exhaustive usage examples.
+///
+/// # Example
+///
+/// ```
+/// #[machine_parameters(
+///     lev: "0..=127" #1,
+///     tun: "-32.0..=32.0" #2,
+///     dec: "0..=127" #3,
+///     hld: "0..=127" #4,
+///     swt: "0..=127" #5,
+///     swd: "0..=127" #6,
+///     wav: "0..=2" #7,
+///     tra: "0..=127" #8,
+/// )]
+/// ```
 #[proc_macro_attribute]
 pub fn machine_parameters(args: TokenStream, input: TokenStream) -> TokenStream {
     let args = parse_macro_input!(args as ParameterArgs);
